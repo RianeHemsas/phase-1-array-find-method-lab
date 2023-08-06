9bfe859b63d50afa797772643198daafdbf19ad7
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-06 13:27:07 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+    
+  ]
+
+  function superbowlWin(array){
+    let newArray= array.find(element =>element.result === "W" );
+   
+  return newArray ?  newArray.year : newArray ;
+  
+}
+
+  console.log(superbowlWin(record));
